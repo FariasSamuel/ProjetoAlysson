c1f4cfac5a5b935117046bc4c722dbe8221b715f
Arduino1607 <samuelfabrasil@gmail.com>
2022-12-19 00:14:10 -0300

    final commit

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,7 @@
 const Post = require('../../modules/Post');
+const checkAuth = require('../../util/check-auth');
+const { AuthenticationError, UserInputError } = require('apollo-server');
+
 module.exports = {
     Query:{
         sayHi(){
@@ -6,12 +9,74 @@ module.exports = {
         },
         async getPosts(){
             try{
-                const posts = await Post.find();
+                const posts = await Post.find().sort({createdAt: -1});
                 return posts;
             }catch(e){
                 throw new Error(e); 
             }
-        }
+        },
         
+        async getPost(_,{postId}){
+            try{
+                const post = await Post.findById(postId);
+                if(post)
+                    return post;
+                else   
+                    throw new Error('Post not found');
+            }catch(e){
+                throw new Error(e);
+            }
+        }
+    },
+    Mutation:{
+        async createPost(_, {body},context){
+            const user = checkAuth(context);
+            const newPost = new Post({
+                body,
+                user: user.id,
+                username: user.username,
+                createdAt: new Date().toISOString()
+            }) 
+
+            const post = await newPost.save();
+
+            return post;
+        },
+        async deletePost(_, { postId }, context) {
+            const user = checkAuth(context);
+      
+            try {
+              const post = await Post.findById(postId);
+              if (user.username === post.username) {
+                await post.delete();
+                return 'Post deleted successfully';
+              } else {
+                throw new AuthenticationError('Action not allowed');
+              }
+            } catch (err) {
+              throw new Error(err);
+            }
+          },
+          async likePost(_, { postId }, context) {
+            const { username } = checkAuth(context);
+      
+            const post = await Post.findById(postId);
+            if (post) {
+              if (post.likes.find((like) => like.username === username)) {
+                // Post already likes, unlike it
+                post.likes = post.likes.filter((like) => like.username !== username);
+              } else {
+                // Not liked, like post
+                post.likes.push({
+                  username,
+                  createdAt: new Date().toISOString()
+                });
+              }
+      
+              await post.save();
+              return post;
+            } else throw new UserInputError('Post not found');
+          },
+          
     }   
 }
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,10 +1,25 @@
 const {gql} = require('apollo-server');
 
 module.exports = gql`
-    type Post{
-        id: ID!,
-        body: String!,
-        createdAt: String!,
+    type Post {
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+        comments: [Comment]!
+        likes: [Like]!
+        likeCount: Int!
+        commentCount: Int!
+    }
+    type Comment{
+        id: ID!
+        createdAt: String!
+        username: String!
+        body: String!
+    }
+    type Like{
+        id: ID!
+        createdAt: String!
         username: String!
     }
     type User{
@@ -22,9 +37,16 @@ module.exports = gql`
     }
     type Query{
         getPosts: [Post],
-        sayHi: String!
+        sayHi: String!,
+        getPost(postId: ID!): Post
     }
     type Mutation{
-        register(registerInput: RegisterInput): User
+        register(registerInput: RegisterInput): User!
+        login(username: String!, password: String!): User!    
+        createPost(body: String!):Post!
+        deletePost(postId: ID!):String!
+        createComment(postId: ID!, body: String!): Post!
+        deleteComment(postId: ID!, commentId: ID!): Post!
+        likePost(postId: ID!):Post!
     }
 `;
